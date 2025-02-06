383c4db71e1ef71d434eb53950b647d423c3d999
Amrzxk <amrzakariya2020@yahoo.com>
2025-02-06 13:07:06 +0200

    second update layed some foundations

--- app.py
+++ app.py
@@ -2,5 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Home page route
+@app.route("/")
+def home():
+    return render_template("index.html")  
 
-app.run(debug=True)
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+# Run the Flask application
+if __name__ == "__main__":
+    app.run(debug=True)
